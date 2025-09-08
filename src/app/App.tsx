3372a572ff69f8a6cbb073df204cc1f88dde8060
Ben Daniel-Greep <ben.daniel-greep@rightmove.co.uk>
2025-09-08 15:14:35 +0100

    add route to invoicecreate to App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import InvoicesList from './components/InvoicesList'
 import InvoiceShow from './components/InvoiceShow'
+import InvoiceCreate from './components/InvoiceCreate'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="container-fluid container-xl mx-auto px-3 px-md-4">
         <Router>
           <Routes>
+            <Route path="/invoice/create" Component={InvoiceCreate} />
             <Route path="/invoice/:id" Component={InvoiceShow} />
             <Route path="/" Component={InvoicesList} />
           </Routes>
